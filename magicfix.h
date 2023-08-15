9e6051e1e3a4e142d5cb375620552533f0328627
Stridsvagn69420 <Stridsvagn69420@users.noreply.github.com>
2023-08-15 14:14:02 +0200

    Change License to EUPL

--- magicfix.h
+++ magicfix.h
@@ -1,8 +1,8 @@
 #ifndef MAGICFIX_GLOBAL_H
 #define MAGICFIX_GLOBAL_H
 
-/// @author Stridsvagn69420
-/// magicfix library entry
+/// @author Stridsvagn69420 
+/// magicfix library
 
 // Submodules
 #include "audio.h"
